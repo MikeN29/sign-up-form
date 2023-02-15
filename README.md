9c4ee9425acc7c0f953d1645afa1e644ddd05d96
Michael Norman <mikenorman192@msn.com>
2023-02-15 19:22:32 +0000

    updated readme

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # sign-up-form
 
-Live preview: https://miken29.github.io/sign-up-form/ 
+Live preview: https://miken29.github.io/sign-up-form/
+
+Outcomes:
+
+- I have created a sign up webpage template
+- i have used Flexbox to position the elements
+- Not responsive
--- scripts.js
+++ scripts.js
@@ -1,16 +1,15 @@
 // if id password doesnt match confirmpassword chang the class to error
 
+// eslint-disable-next-line no-unused-vars
 function checkPasswords() {
-    let password = document.getElementById("password").value;
-    let confirmpassword = document.getElementById("confirmPassword").value;
+  const password = document.getElementById("password").value;
+  const confirmpassword = document.getElementById("confirmPassword").value;
 
-if (password != confirmpassword) {
-    document.getElementById("password").classList.add('error');
-    document.getElementById("confirmPassword").classList.add('error');
-} else {
-    document.getElementById("password").classList.remove('error');
-    document.getElementById("confirmPassword").classList.remove('error');
+  if (password !== confirmpassword) {
+    document.getElementById("password").classList.add("error");
+    document.getElementById("confirmPassword").classList.add("error");
+  } else {
+    document.getElementById("password").classList.remove("error");
+    document.getElementById("confirmPassword").classList.remove("error");
+  }
 }
-}
-
-
